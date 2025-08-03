2401836ce3c5b4109ee35a0617367a5bbb27f75a
Sisal Acharya <sisalacharya13@gmail.com>
2025-08-03 19:07:49 +0545

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const page = () => {
+  return (
+    <div>
+      <Button variant="ghost" size="lg" className="bg-red-500 text-white p-4 rounded">
+        Click Mee
+      </Button>
+    </div>
+  )
+}
+
+export default page
